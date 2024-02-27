da8fd18f58906430dcdc50ad94213d266374f7fc
hasunwoo <whs1999kr@naver.com>
2024-02-27 21:32:16 +0900

    added nvim-tree

---
+++ lua/plugins/nvim_tree_config.lua
@@ -0,0 +1,35 @@
+local function config_nvim_tree()
+    -- disable netrw at the very start of your init.lua
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    require("nvim-tree").setup({
+        sort = {
+            sorter = "case_sensitive",
+        },
+        view = {
+            width = 30,
+        },
+        renderer = {
+            group_empty = true,
+        },
+        filters = {
+            dotfiles = true,
+        },
+    })
+
+    local api = require("nvim-tree.api")
+
+    -- toggle nvim-tree
+    vim.keymap.set('n', '<leader>fe', api.tree.toggle, {})
+end
+
+return {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = config_nvim_tree
+}
